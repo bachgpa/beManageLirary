13c6b644ab5334cf4aa2e09d7e3e13037febd1a2
Bách Nguyễn <105241006+bachgpa@users.noreply.github.com>
2024-02-04 23:18:22 +0700

    copy

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const hbs = require("express-handlebars");
+const path = require("path");
+const exp = require("constants");
+
+const port = 2000;
+const app = express();
+
+app.use(morgan("combined"));
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./views"));
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.use("/", (req, res) => {
+  // res.send("hello world");
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`App listen at port: http://localhost:${port}`);
+});
